4c3cced23c725d3e19409eb07afd2d6b5907bcd8
paaaarv <pkumar@trinity.edu>
2021-11-24 23:26:14 -0600

    solved two algorithm problems

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 // Code your solution here!
 
+
+function printString(string){
+    let mySubstring;
+    console.log(string[0])
+    if (string.length >1){
+        mySubstring=string.substring(1, string.length);
+        printString(mySubstring)
+    }else{
+        return true
+    }
+}
+
+
+function reverseString(string){
+    if (string.length<=1){
+        return string;
+    }
+    else{
+        return string.charAt(string.length-1) + reverseString(string.substring(0,string.length-1))
+    }
+}
