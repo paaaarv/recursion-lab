309168d5bc2744f60b488a10898846ce9de89bfc
paaaarv <pkumar@trinity.edu>
2021-11-27 12:31:56 -0600

    finished all algorithms

--- index.js
+++ index.js
@@ -12,12 +12,70 @@ function printString(string){
     }
 }
 
-
 function reverseString(string){
     if (string.length<=1){
         return string;
     }
     else{
         return string.charAt(string.length-1) + reverseString(string.substring(0,string.length-1))
+    }
+}
+
+function isPalindrome(string){
+    let subString;
+    if(string.length<2){
+        return true;
+    }
+    else{
+        if(string[0]===string[string.length-1]){
+
+            subString=string.substring(1,string.length-1)
+            isPalindrome(subString)
+        }else{
+            return false;
+        }
+    }
+    return true;
+}
+
+function addUpTo(array, num,total=0){
+    let newArray;
+    if(num<=0){
+        return array[0]
+    }
+    else{
+        array[1]+=array[0]
+        newArray=array.slice(1, array.length)
+        return addUpTo(newArray, num-1, total)
+    }
+}
+
+function maxOf(array){
+    let newArray;
+    if(array.length<=1){
+        return array[0]
+    }
+    else{
+        if(array[1]>array[0]){
+            newArray=array.slice(1)
+            return maxOf(newArray)
+        }
+        if(array[0]> array[1]){
+            array.pop()
+            return maxOf(array)
+        }
+    }
+}
+
+
+function includesNumber(array,num){
+    if(array[0]===num){
+        return true
+    }
+    else if (array.length >0){
+        array.shift()
+        return includesNumber(array, num)
+    }else{
+        return false
     }
 }
